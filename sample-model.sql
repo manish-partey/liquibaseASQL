c3cb27d68756d75b6a05df4bd7c22ea1aa5472d4
unknown <manishk.partey@gmail.com>
2022-10-13 19:36:12 +0000

    Updated DB SQL

--- sample-model.sql
+++ sample-model.sql
@@ -93,8 +93,11 @@ CREATE TABLE stocks (
 	FOREIGN KEY (product_id) REFERENCES products (product_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+DROP VIEW IF EXISTS Product_In_Store;
+GO
 CREATE OR ALTER VIEW Product_In_Store 
 AS 
 SELECT P.product_name, P.list_price, B.brand_name
 FROM products P
 INNER JOIN brands B ON P.brand_ID = B.brand_ID;
+GO
